6ba2c5de513720af3c047da12d3d2bff6bfbf534
Aniket524 <aniketkarp524@gmail.com>
2024-08-10 19:26:07 +0530

    created the login and signup frontend UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='p-4 flex items-center justify-center h-screen'>
-        <Login />
+        <Signup />
     </div>
   )
 }
